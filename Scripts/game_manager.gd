e8457436c26ab9f1a9e91083c0bdc03e2c1698b4
Jpergon251 <114741595+Jpergon251@users.noreply.github.com>
2024-11-27 12:46:39 +0100

    Revert "Merge branch 'Develop-New' into MarioValverde1"
    This reverts commit 753d66b039fbbb92a515c8693e3adc14f03ebeae, reversing
    changes made to 995fe4e5692baa6b745bf71501eddfc45b336c97.

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,6 +1,6 @@
 # GameManager.gd
 extends Node
-const PLAYER_DAMAGE = preload("res://Shaders/Player_damage.gdshader")
+
 ##--------------- Estadiscticas del jugador ------------------
 var player_health = MAX_HEALTH
 var score: int = 0
@@ -14,21 +14,19 @@ var visible_e_key = false
 var teleport_activate : bool = false  # Variable para controlar si el teletransporte está activado
 var teleport_destination : String = ""  # Esta es la propiedad que necesitas para almacenar el destino del teletransporte
 var player_node: CharacterBody2D
-var spawn_point: Vector2
+
 ##--------- PowerUps activables ---------
 
 var double_jump: bool = false
-var dash: bool = true
+var dash: bool = false
 var flashlight: bool = false
 
 ##--------- Variables escena 1 ---------
 var player_position_puzzle: Vector2
 var puzzle_1_complete = false
-var occupied_positions_puzle_1: Array[Vector2] = []
-
 var puzzle_2_complete = false
 var puzzle_3_complete = false
-var puzzle_3_position: Vector2 
+
 func apply_push_to_player(enemy_position: Vector2, player: CharacterBody2D) -> void:
 	push_direction = player.position - enemy_position  # Dirección del empuje
 	push_direction = push_direction.normalized()  # Normalizamos para mantener el empuje constante
@@ -44,11 +42,6 @@ func take_player_damage(body: Node) -> void:
 	print("Jugador recibe daño")
 	if player_health > 0:
 		player_health -= 1
-		#Aplica el efecto de daño
-		body.can_take_damage = false
-		body.i_frames.start()
-		body.get_child(0).material.set_shader_parameter("mix_color",0.7)
-		body.damage_timer.start()
 		print("Vida restante del jugador:", player_health)
 		if player_health <= 0:
 			print("¡El jugador ha muerto!")
