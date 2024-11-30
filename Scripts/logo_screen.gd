121a0842466d48bfd9427ae12a32ca5bbc301189
Jose Antonio Perez Gonzalez <114741595+Jpergon251@users.noreply.github.com>
2024-11-30 18:38:19 +0100

    Cambios en algunos parametros

--- Scripts/logo_screen.gd
+++ Scripts/logo_screen.gd
@@ -1,6 +1,6 @@
 extends Node2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var karambit_studios_logo_contorno_blanco_: TextureRect = $"KarambitStudiosLogo(contornoBlanco)"
+@onready var karambit_studios_logo_contorno_blanco_: Sprite2D = $"KarambitStudiosLogo(contornoBlanco)"
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "logo_animation":
